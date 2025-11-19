21937bfc242be6fb35fdf82e73cd7704c3386c02
toyoshima ryo <2311140053m@ed.fuk.kindai.ac.jp>
2025-11-19 09:02:57 +0000

    ノート新規追加機能追加

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,4 +2,16 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
   end
+  
+  def new
+    @note = Note.new
+  end
+  
+  def create
+    note = Note.new(user_id: nil, title: params[:note][:title], content: params[:note][:content])
+    note.save
+    redirect_to '/'
+  end
+  
+  
 end
