ce8904893afd346e53c6d039d2b998b3e3595ac6
Mineyu <2311140072d@ed.fuk.kindai.ac.jp>
2025-12-17 06:15:31 +0000

    fix: 非ログイン時にマイページへのリンクを表示しないように変更 ログインせずにマイページのリンクを踏むとエラーが発生するため、ログイン時のみマイページへのリンクを表示するよう変更

--- db/schema.rb
+++ db/schema.rb
@@ -89,7 +89,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_10_071726) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
